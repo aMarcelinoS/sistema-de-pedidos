db4a8059e0b4cb2ddbf7ed59e5c7ec1e5122a08a
Alexandre Marcelino <amsantos9@gmail.com>
2022-04-08 16:02:20 -0300

    Projeto criado

---
+++ backend-df/src/main/java/br/com/alexandre/backenddf/BackendDfApplication.java
@@ -0,0 +1,13 @@
+package br.com.alexandre.backenddf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDfApplication.class, args);
+	}
+
+}
